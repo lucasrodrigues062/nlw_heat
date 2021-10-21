43df4c417e96f6b8a71ee7903811c3593d7e688c
Lucas Rodrigues <lucasrodrigues062@gmail.com>
2021-10-21 15:40:56 -0300

    concluido aula 3 com falha na autenticacao

---
+++ nlwheatapp/src/components/Header/styles.ts
@@ -0,0 +1,27 @@
+import { COLORS } from './../../theme/colors';
+import { StyleSheet } from 'react-native';
+import { FONTS } from '../../theme';
+
+export const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingHorizontal: 20,
+  },
+
+  logoutText: {
+    fontSize: 15,
+    fontFamily: FONTS.REGULAR,
+    color: COLORS.WHITE,
+    marginRight: 20
+  },
+
+  logoutButton: {
+    flexDirection: 'row',
+    alignItems: 'center'
+  }
+
+
+});
